d3a35eda1200789ff07baad44e7c6059adc68908
e <ernesto@MacBook-Air-de-ernesto.local>
2018-05-05 22:41:05 -0500

    caca

--- sudokaka/fuck.py
+++ sudokaka/fuck.py
@@ -84,6 +84,98 @@ def candidates_to_str(c):
     return "\n".join(r)
 
 
+def check_uniq_definitory_row(c, idx, ri, rs):
+    ii, ij = square_idx_to_pos(idx)
+    u = True
+    for i in range(3):
+        ci = ii + i
+        if i != ri:
+            for j in range(3):
+                cj = ij + j
+                cs = c[ci][cj]
+                if rs <= cs:
+                    u = False
+                    break
+            if not u:
+                break
+    return u
+
+
+def check_uniq_definitory_col(c, idx, rj, rs):
+    ii, ij = square_idx_to_pos(idx)
+    u = True
+    for j in range(3):
+        cj = ij + j
+        if j != rj:
+            for i in range(3):
+                ci = ii + i
+                cs = c[ci][cj]
+                if rs <= cs:
+                    u = False
+                    break
+            if not u:
+                break
+    return u
+
+
+def get_definitory_lines(c, idx):
+    ii, ij = square_idx_to_pos(idx)
+    rows = {}
+    cols = {}
+    for i in range(3):
+        ci = ii + i
+        rs = None
+        ps = []
+        for j in range(3):
+            cj = ij + j
+            cs = c[ci][cj]
+            if len(cs) > 1:
+                if rs is None:
+                    rs = set(cs)
+#                print("ci {} cj {} cs {} rs {}".format(ci, cj, candidate_set_to_str(cs), candidate_set_to_str(rs)))
+                rs &= cs
+                ps.append((ci, cj))
+        if rs and check_uniq_definitory_row(c, idx, i, rs) and len(rs) <= len(ps):
+#            print("g ci {} rs {}".format(ci, candidate_set_to_str(rs)))
+            rows[ci] = [rs, ps]
+            
+#    print("r1 rows {}".format(rows))
+    for j in range(3):
+        cj = ij + j
+        rs = None
+        ps = []
+        for i in range(3):
+            ci = ii + i
+            cs = c[ci][cj]
+            if len(cs) > 1:
+                if rs is None:
+                    rs = set(cs)
+                rs &= cs
+                ps.append((ci, cj))
+        if rs and check_uniq_definitory_col(c, idx, j, rs) and len(rs) <= len(ps):
+            cols[cj] = [rs, ps]
+#    print("r rows {}".format(rows))
+    return rows, cols
+
+
+def process_definitory_lines(c):
+    rows = {}
+    cols = {}
+    for idx in range(9):
+        rowst, colst = get_definitory_lines(c, idx)
+        print(" idx {} r {} c {}".format(idx, list(map(lambda x:candidate_set_to_str(rowst[x][0]) + ":" + str(rowst[x][1]), rowst)), list(map(lambda x:candidate_set_to_str(colst[x][0]) + ":" + str(colst[x][1]), colst))))
+        rows.update(rowst)
+        cols.update(colst)
+    
+    for i in rows:
+        s, e = rows[i]
+        remove_candidates_row(c, s, i, e)
+    
+    for j in cols:
+        s, e = cols[j]
+        remove_candidates_col(c, s, j, e)
+    
+
 class Solution:
 
     def solveSudoku(self, board):
@@ -93,6 +185,8 @@ class Solution:
         """
         c = create_candidates(board)
         init_candidates(c)
+        print(candidates_to_str(c))
+        process_definitory_lines(c)
         print(candidates_to_str(c))
 
 
